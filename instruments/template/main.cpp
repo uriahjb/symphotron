8967660468fa31ae59c21cd8ca83ff36c1ff8417
Uriah Baalke <uriahjb@gmail.com>
2013-11-05 21:15:59 -0500

    added support for m4, an instruments directory, and a template with Makefile

---
+++ instruments/template/main.cpp
@@ -0,0 +1,37 @@
+// These are C things extern C so we can mix C and CPP
+extern "C" {
+#include "mGeneral.h"
+#include "mBus.h"
+#include "mUSB.h"
+}
+
+#include "bqueue.h"
+#include "usb_iface.h" 
+
+int main( void )
+{
+    // Initialize m4 things
+    mInit();
+    //mBusInit();
+    mUSBInit();
+
+    // Set up usb interface
+    bQueue in_q;
+    bQueue out_q;
+    usbIface usb( &in_q, &out_q );
+
+    mYellowON;
+    
+    uint8_t cnt = 0;
+    while(1)
+    {
+        mWaitms(500);
+        mGreenON;
+        usb.printf( "hello world\n" );
+        mWaitms(500);
+        mGreenOFF;
+        
+        // Write data out 
+        usb.writeBytes();
+    }
+}
